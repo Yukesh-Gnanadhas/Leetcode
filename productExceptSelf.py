79e92d34440f5d0406de387b7efd7ab5d50f6ae1
Yukesh-Gnanadhas <151903060+Yukesh-Gnanadhas@users.noreply.github.com>
2026-06-24 18:48:03 +0900

    Update productExceptSelf.py

--- productExceptSelf.py
+++ productExceptSelf.py
@@ -1,6 +1,6 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # using prefix and suffix products
+        # storing prefix in result array and then multiplying them with the postfix
         res=[1]*(len(nums))
         prefix = 1
         for i in range(len(nums)):
@@ -13,3 +13,20 @@ class Solution:
         
         return res
 
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # using prefix and suffix products
+        prefix=[1]*(len(nums))
+        postfix=[1]*(len(nums))
+        res= [1]*(len(nums))
+        for i in range(1,len(nums)):
+            prefix[i]=prefix[i-1]*nums[i-1]
+
+        for i in range(len(nums)-2,-1,-1):
+            postfix[i] = postfix[i+1]*nums[i+1]
+   
+        for i in range(len(nums)):
+            res[i]= prefix[i]*postfix[i]
+        
+        return res
